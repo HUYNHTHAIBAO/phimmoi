c6dde00276100858da8e07a2b2512d96be14e5e5
Huỳnh Thái Bảo <huynhthaibao.it@gmail.com>
2022-08-23 17:51:37 +0700

    response

--- app.js
+++ app.js
@@ -1,33 +1,33 @@
 
 
 
-function creat(title = "", desc = "") {
-    const template = ` <div class="model">
-    <img src="./assets/image/logo.png" alt="" class="model-img">
-    <div class="model-desc">
-         <h3 class="model-name">${title}</h3>
-         <p class="model-text">Đang xem phim</p>
-    </div>
-</div>`;
-document.body.insertAdjacentHTML("beforebegin", template);
-}
+// function creat(title = "", desc = "") {
+//     const template = ` <div class="model">
+//     <img src="./assets/image/logo.png" alt="" class="model-img">
+//     <div class="model-desc">
+//          <h3 class="model-name">${title}</h3>
+//          <p class="model-text">Đang xem phim</p>
+//     </div>
+// </div>`;
+// document.body.insertAdjacentHTML("beforebegin", template);
+// }
 
-const randomData = [
+// const randomData = [
  
-    "Huỳnh Thái Bảo", 
-    "Nguyễn Thanh Bình",
-    "Ăn chơi",
-    "thanh nguyen",
+//     "Huỳnh Thái Bảo", 
+//     "Nguyễn Thanh Bình",
+//     "Ăn chơi",
+//     "thanh nguyen",
 
-  ];
+//   ];
 
-let last;
-const timer = setInterval(function()  { 
-    const item = document.querySelector(".model");
-    if(item) item.parentNode.removeChild(item);
-    const title = randomData[Math.floor(Math.random() *randomData.length)];
-    if(last !==  title) {
-        creat(title);
-    }
-    last = title;
-}, 5000)
+// let last;
+// const timer = setInterval(function()  { 
+//     const item = document.querySelector(".model");
+//     if(item) item.parentNode.removeChild(item);
+//     const title = randomData[Math.floor(Math.random() *randomData.length)];
+//     if(last !==  title) {
+//         creat(title);
+//     }
+//     last = title;
+// }, 5000)
